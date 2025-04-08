f9a5b982da6153f266a2eae350eb3d337169e72e
Thi So Ny Nguyen <tngu1033@my.centennialcollege.ca>
2025-04-08 15:58:04 -0400

    Initial commit: Added CalculatorApp solution with CalculatorLib and CalculatorTests

---
+++ MyCalculatorApp/CalculatorTests/CalculatorTests.cs
@@ -0,0 +1,46 @@
+using CalculatorLib;
+using Xunit;
+
+namespace CalculatorTests
+{
+    public class CalculatorTests
+    {
+        private readonly Calculator _calculator;
+
+        public CalculatorTests()
+        {
+            _calculator = new Calculator();
+        }
+
+        [Fact]
+        public void Add_TwoNumbers_ReturnsSum()
+        {
+            Assert.Equal(5, _calculator.Add(2, 3));
+        }
+
+        [Fact]
+        public void Subtract_TwoNumbers_ReturnsDifference()
+        {
+            Assert.Equal(3, _calculator.Subtract(5, 2));
+        }
+
+        [Fact]
+        public void Multiply_TwoNumbers_ReturnsProduct()
+        {
+            Assert.Equal(12, _calculator.Multiply(4, 3));
+        }
+
+        [Fact]
+        public void Divide_TwoNumbers_ReturnsQuotient()
+        {
+            Assert.Equal(4, _calculator.Divide(8, 2));
+        }
+
+        [Fact]
+        public void Divide_ByZero_ThrowsException()
+        {
+            var exception = Assert.Throws<ArgumentException>(() => _calculator.Divide(5, 0));
+            Assert.Equal("Cannot divide by zero.", exception.Message);
+        }
+    }
+}
---
+++ MyCalculatorApp/MyCalculatorApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using CalculatorLib;
+
+namespace CalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calc = new Calculator();
+            Console.WriteLine("Calculator App");
+            Console.WriteLine($"2 + 3 = {calc.Add(2, 3)}");
+            Console.WriteLine($"5 - 2 = {calc.Subtract(5, 2)}");
+            Console.WriteLine($"4 * 3 = {calc.Multiply(4, 3)}");
+            Console.WriteLine($"8 / 2 = {calc.Divide(8, 2)}");
+        }
+    }
+}
